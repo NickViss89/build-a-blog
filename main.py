3dd1f627a00e8008400c2ef299f6d28c9b7b3a0f
Nick Visser <nickvisser89@gmail.com>
2018-04-12 21:46:21 -0500

    skeleton code

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog1:password@localhost:8889/build-a-blog1'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Blog(db.Model):
+class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(500))
+    name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, name):
+        self.name = name
         self.completed = False
 
 
@@ -24,27 +23,27 @@ class Blog(db.Model):
 def index():
 
     if request.method == 'POST':
-        post_name = request.form['post']
-        new_task = Post(post_name)
-        db.session.add(new_post)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
         db.session.commit()
 
-    posts = Task.query.filter_by(completed=False).all()
-    completed_posts = Task.query.filter_by(completed=True).all()
-    return render_template('post.html',title="Blog it!", 
-        posts=posts, completed_posts=completed_posts)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
 
 
-@app.route('/new-post', methods=['POST'])
-def new_post():
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
 
-    post_id = int(request.form['task-id'])
-    post = Post.query.get(post_id)
-    post.completed = True
-    db.session.add(post)
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
-    return redirect('/all-posts')
+    return redirect('/')
 
 
 if __name__ == '__main__':
