9dd957f0b51b7ccf24e9fd157f2cbe08f0883fdd
Nick Visser <nickvisser89@gmail.com>
2018-04-14 18:54:29 -0500

    its working!

--- main.py
+++ main.py
@@ -24,26 +24,37 @@ def index():
     return redirect('/blog')
 
    
-@app.route("/newpost", methods=['GET', 'POST'])
+@app.route("/newpost", methods=['POST', 'GET'])
 def blog_entries():
-    if request.method == 'POST':    
+
+    if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
-
-    title = Blog.query.all()
-    body = Blog.query.all()  
+        req_error = ""
+        if len(blog_title) == 0 or len(blog_body) == 0:
+            req_error = "Both fields required"
+        if req_error == "":
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            post = new_blog.id
+            return redirect('/blog?id=' + str(post))
+        else:
+            return render_template('new_post.html', req_error=req_error)
     return render_template('new_post.html')
-        
+      
 @app.route("/blog", methods=['GET', 'POST'])
 def blog():
 
-    title = Blog.query.get('title')
-    body = Blog.query.get('body')
-    posts = Blog(title, body)
-    return render_template("blog.html", title=title, body=body)
+    posts = Blog.query.all()
+    id = request.args.get('id')
+    unique_id = Blog.query.filter_by(id=id).first()
+    if not unique_id:
+        return render_template("blog.html", posts=posts)
+    else:
+        return render_template("single_post.html", post=unique_id)
+
+
 
 
 if __name__ == '__main__':
